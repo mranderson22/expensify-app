2b5bae9010efdab0c2463cdc6a072f36e823695a
Eric Anderson <anderson.eda@gmail.com>
2019-04-23 11:27:39 -0400

    firebase to redux

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,21 +15,128 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-firebase.database().ref().set({
-  name: 'Eric Anderson',
-  age: 31,
-  isSingle: false,
-  location: {
-    city: 'Charlotte',
-    country: 'United States'
-  }
-});
+export { firebase, database as default };
 
-// firebase.database().ref().set('This is my data.')
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-database.ref('age').set(32);
-database.ref('location/city').set('Greenville')
-database.ref('attributes').set({
-  height: 77,
-  weight: 215
-});
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// })
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+// });
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+//   });
+
+// database.ref('expenses').push({
+//   description: 'yoga',
+//   note: 'hot yoga class',
+//   amount: '5000',
+//   createdAt: '4/22/2019'
+// });
+
+// database.ref('notes').set(notes);
+
+
+
+// database.ref().on('value', (snapshot) => {
+//   const data = snapshot.val();
+//   console.log(`${data.name} is a ${data.job.title} at ${data.job.company}`);
+// });
+
+// setTimeout(() => {
+//   database.ref().update({
+//     name: 'Alana Anderson',
+//     'job/title': 'Engineering Manager'
+//   })
+// }, 4000)
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// }, (e) => {
+//   console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//   database.ref('age').set(20);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(37);
+// }, 10500);
+
+// database.ref('location/city')
+// .once('value')
+// .then((snapshot) => {
+//   const val = snapshot.val();
+//   console.log(val);
+// })
+// .catch((e) => {
+//   console.log('Error fetching data', e)
+// })
+
+// firebase.database().ref().set({
+//   name: 'Eric Anderson',
+//   age: 31,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'Charlotte',
+//     country: 'United States'
+//   }
+// }).then(() => {
+//   console.log('data is saved!!');
+// }).catch((error) => {
+//   console.log('this failed!', error);
+// });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
+
+// database.ref()
+// .remove()
+// .then(() => {
+//   console.log('success!')
+// })
+// .catch((e) => {
+//   console.log('error!', e)
+// })
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,9 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
@@ -18,33 +23,42 @@ test('should setup edit expense action object', () => {
 });
 
 test('should setup add expense action object with provided values', () => {
-  const expenseData = {
-    description: 'rent',
-    amount: 170000,
-    createdAt: 1000,
-    note: 'This was last month\'s rent'
-  };
-  const action = addExpense(expenseData);
+  const action = addExpense(expenses[2]);
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
-    expense: {
-      ...expenseData,
-      id: expect.any(String)
-    }
+    expense: expenses[2]
   })
 });
 
-test('should setup the add expense action object with default values', () => {
-  const action = addExpense();
-  expect(action).toEqual({
-    type: 'ADD_EXPENSE',
-    expense: {
-      description: '',
-      note: '',
-      amount: 0,
-      createdAt: 0,
-      id: expect.any(String)
-    }
-  })
+test('should add expenses to database and store', (done) => {
+  const store = createMockStore({});
+  const expenseData = {
+    description: 'mouse',
+    amount: 3000,
+    note: 'THis one is better',
+    createdAt: 1000
+  };
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    expect(1).toBe(1);
+    done();
+  });
 });
+
+test('should add expenses with defaults to database and store', () => {
+
+});
+
+// test('should setup the add expense action object with default values', () => {
+//   const action = addExpense();
+//   expect(action).toEqual({
+//     type: 'ADD_EXPENSE',
+//     expense: {
+//       description: '',
+//       note: '',
+//       amount: 0,
+//       createdAt: 0,
+//       id: expect.any(String)
+//     }
+//   })
+// });
 
